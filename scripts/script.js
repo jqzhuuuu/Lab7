5fadf7fbe4ba02ddb0c619b768e917462775b5a8
jqzhuuuu <joshzhu12@gmail.com>
2021-05-13 21:33:27 -0700

    update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,7 @@
 import { router } from './router.js'; // Router imported so you can use it to manipulate your SPA app here
 const setState = router.setState;
 
+var counter = 0;
 // Make sure you register your service worker here too
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -10,7 +11,12 @@ document.addEventListener('DOMContentLoaded', () => {
     .then(response => response.json())
     .then(entries => {
       entries.forEach(entry => {
+        counter++;
         let newPost = document.createElement('journal-entry');
+        newPost.addEventListener('click', () => {
+          router.setState(counter);
+        });
+        //console.log(counter);
         newPost.entry = entry;
         document.querySelector('main').appendChild(newPost);
       });
