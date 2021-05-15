64d79a918da19fca51b7d561b82cca7a3128f3a7
jqzhuuuu <joshzhu12@gmail.com>
2021-05-14 21:34:46 -0700

    implement sw.js

--- sw.js
+++ sw.js
@@ -4,3 +4,53 @@
 //   - One for installation
 //   - One for activation ( check out MDN's clients.claim() for this step )
 //   - One for fetch requests
+
+//registering service worker
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/sw.js').then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
+
+//install service worker
+var CACHE_NAME = 'Lab7-cache';
+var urlsToCache = [
+  'https://cse110lab6.herokuapp.com/entries'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+//activation
+self.addEventListener('activate', event => {
+  event.waitUntil(clients.claim());
+});
+
+//fetching
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
