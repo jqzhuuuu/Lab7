68bacbb82bcdd02a70284183af4b8440a78629f5
jqzhuuuu <joshzhu12@gmail.com>
2021-05-14 21:39:06 -0700

    update sw.js

--- sw.js
+++ sw.js
@@ -6,17 +6,17 @@
 //   - One for fetch requests
 
 //registering service worker
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-      navigator.serviceWorker.register('/sw.js').then(function(registration) {
-        // Registration was successful
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      }, function(err) {
-        // registration failed :(
-        console.log('ServiceWorker registration failed: ', err);
-      });
-    });
-  }
+// if ('serviceWorker' in navigator) {
+//     window.addEventListener('load', function() {
+//       navigator.serviceWorker.register('/sw.js').then(function(registration) {
+//         // Registration was successful
+//         console.log('ServiceWorker registration successful with scope: ', registration.scope);
+//       }, function(err) {
+//         // registration failed :(
+//         console.log('ServiceWorker registration failed: ', err);
+//       });
+//     });
+//   }
 
 //install service worker
 var CACHE_NAME = 'Lab7-cache';
